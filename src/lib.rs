25ee8fb47d733a06774ff81f7cfcacddafda0644
Christian Lewe <ch_lewe@agdsn.me>
2024-06-05 22:23:30 +0200

    Export shinkyuu functions
    Add example usage to doc test

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,13 @@
 //! Ready for WASM 🌀
 //!
 //! ```
-//! use konbaato::{hira_to_kata, kata_to_hira, vu_to_bu};
+//! use konbaato::{hira_to_kata, kata_to_hira, vu_to_bu, kakushin_to_kyuu, kyuu_to_kakushin};
 //!
-//! assert_eq!("カタカナ", hira_to_kata("かたかな"));
-//! assert_eq!("ひらがな", kata_to_hira("ヒラガナ"));
-//! assert_eq!("バイオリン", vu_to_bu("ヴァイオリン"));
+//! assert_eq!("カタカナ", &hira_to_kata("かたかな"));
+//! assert_eq!("ひらがな", &kata_to_hira("ヒラガナ"));
+//! assert_eq!("バイオリン", &vu_to_bu("ヴァイオリン"));
+//! assert_eq!("撹拌", &kyuu_to_kakushin("攪拌"));
+//! assert_eq!("儘ならぬ", &kakushin_to_kyuu("侭ならぬ"));
 //! ```
 
 mod hirakata;
@@ -16,6 +18,7 @@ mod shinkyuu;
 mod vubu;
 
 pub use hirakata::{hira_to_kata, kata_to_hira};
+pub use shinkyuu::{kakushin_to_kyuu, kyuu_to_kakushin};
 pub use vubu::vu_to_bu;
 
 #[cfg(test)]
